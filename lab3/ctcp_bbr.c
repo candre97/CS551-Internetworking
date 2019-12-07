0dfc53b382731a6b9fa38d1ac6b46b87bb2c975f
cs144 <foo@bar.com>
2019-12-06 17:26:44 -0800

    stripped down code to bare essentials. Did not implement stuff that was confusing.

--- lab3/ctcp_bbr.c
+++ lab3/ctcp_bbr.c
@@ -2,175 +2,8 @@
 
 #include "ctcp_bbr.h"
 
-void BBRUpdateModelAndState(bbr_t* bbr, uint32_t delivery_rate) {
-	/* calculate delivery rate */
-	BBRUpdateBtlBw(bbr_t* bbr, delivery_rate);
-    BBRCheckCyclePhase(bbr_t* bbr);
-    BBRCheckFullPipe(bbr_t* bbr);
-    BBRCheckDrain(bbr_t* bbr);
-    BBRUpdateRTprop(bbr_t* bbr);
-    BBRCheckProbeRTT(bbr_t* bbr);
-}
+void bbr_init(bbr_t* bbr) {
 
-void BBRUpdateBtlBw(bbr_t* bbr, uint32_t delivery_rate) {
-	BBRUpdateRound(bbr);
-	if (bbr->delivery_rate >= bbr->btl_bw || !bbr->is_app_limited) {
-	    bbr->BtlBw = max(delivery_rate, bbr->btl_bw);
-	}
-}
-
-void BBRInitPacingRate(bbr_t* bbr, uint16_t InitialCwnd) {
-	int nominal_bandwidth = InitialCwnd / (SRTT ? SRTT : 1);
-    bbr->pacing_rate =  bbr->pacing_gain * nominal_bandwidth;
-    bbr->pacing_rate_init = true;
-}
-
-void BBRSetPacingRate(bbr_t* bbr, ctcp_segment_t* segment) {
-	
-	if(!bbr->pacing_rate_init) {
-		BBRInitPacingRate(bbr, ntohs(segment->window));
-		return;
-	}
-
-	/* 	
-		To adapt to the bottleneck, in general BBR sets the 
-		pacing rate to be proportional to BBR.BtlBw, with a dynamic gain, 
-		or scaling factor of proportionality, called pacing_gain.
-	*/
-	float rate;
-	rate = bbr->pacing_gain * bbr->btl_bw;
-	if(bbr->filled_pipe || (rate > bbr->pacing_rate)) {
-		bbr->pacing_rate = rate;
-	}
-
-}
-
-void BBRModulateCwndForProbeRTT(bbr_t* bbr) {
-	if (bbr->mode == BBR_Probe_RTT) {
-    	bbr->cwnd = min(bbr->cwnd, 4);
-	}
-}
-
-
-uint16_t BBRSaveCwnd(bbr_t* bbr) {
-	if (/*not InLossRecovery()*/ bbr->bbr_mode != BBR_PROBE_RTT) {
-    	return bbr->cwnd;
-	}
-    else {
-    	return max(bbr->prior_cwnd, cwnd);
-    }
-}
-
-void BBRUpdateOnRTO(bbr_t* bbr) {
-	bbr->prior_cwnd = BBRSaveCwnd(bbr);
-	bbr->cwnd = 1;
-	bbr->packet_conservation = true; 
-}
-
-/* BBR.
-TODO: implement these functions: 
-void uponEnteringFastRecovery()
-  BBR.prior_cwnd = BBRSaveCwnd()
-  cwnd = packets_in_flight + max(packets_delivered, 1)
-  BBR.packet_conservation = true
-
-void onACKinFastRecovery()
-BBRModulateCwndForRecovery():
-    if (packets_lost > 0)
-      cwnd = max(cwnd - packets_lost, 1)
-    if (BBR.packet_conservation)
-      cwnd = max(cwnd, packets_in_flight + packets_delivered)
-
-After one trip in fast recovery: 
-  BBR.packet_conservation = false
-
-After exiting fast recovery:
-	BBR.packet_conservation = false
-	BBRRestoreCwnd()
-*/
-
-void BBRSetSendQuantum(bbr_t* bbr) {
-	fprintf(stderr, "Pacing Rate %d\n", bbr->pacing_rate);
-	int MBps = 10^3/8; /* scaled to mbp ms */
-	if (bbr->pacing_rate < 1.2 * MBps) {
-		bbr->send_quantum = 1 * SMSS;
-	}
-    else if (bbr->pacing_rate < 24 * MBps) {
-      	bbr->send_quantum  = 2 * SMSS;
-    }
-    else { /* Bytes  */
-      	bbr->send_quantum  = min(bbr->pacing_rate * 1, 64000)
-    }
-}
-
-void BBRSetCwnd(bbr_t* bbr, uint16_t ackd_packets) {
-	BBRUpdateTargetCwnd(bbr);
-	/* modulateCwndForRecovery() */
-	if(!bbr->packet_conservation) {
-		if(bbr->filled_pipe) {
-			bbr->cwnd = min(bbr->cwnd + ackd_packets, bbr->target_cwnd); 
-		}
-		else if(bbr->cwnd < bbr->target_cwnd || bbr->delivered < bbr->initial_cwnd) {
-			bbr->cwnd += ackd_packets; 
-		}
-		/* impose a floor on cwnd */
-		bbr->cwnd = max(bbr->cwnd, BBRMinPipeCwnd);
-	}
-	bbr->packet_conservation = false;
-	BBRModulateCwndForProbeRTT(bbr);
-}
-
-uint16_t BBRInFlight(bbr_t* bbr, float gain) {
-	if(bbr->rt_prop < 0) {
-		bbr->target_cwnd = bbr->initial_cwnd;
-		return;
-	}
-	uint32_t quanta = 3 * bbr->send_quantum;
-	uint32_t est_bdp = bbr->btl_bw * bbr->rt_prop;
-	return ((est_bdp * bbr->cwnd_gain) + quanta) / SMSS;  
-	/* to go from bytes to a multiplier of SMSS*/
-}
-
-void BBRUpdateTargetCwnd(bbr_t* bbr) {
-	bbr->target_cwnd == BBRInFlight(bbr->cwnd_gain); 
-	return;
-}
-
-void BBRUpdateControlParameters(bbr_t* bbr, ctcp_segment_t* segment, uint16_t ackd_packets) {
-	bbr->next_send_time = current_time() + (ntohs(segment->len) / bbr->pacing_rate);
-	BBRSetPacingRate(bbr);
-    BBRSetSendQuantum(bbr);
-    BBRSetCwnd(bbr, ackd_packets);
-}
-
-void BBRUpdateRound(bbr_t* bbr, ctcp_state_t* state, ctcp_segment_t* segment) {
-	bbr->delivered += ntohs(segment->len);
-	if(segment->seqno >= bbr->next_round_delivered) {
-		bbr->next_round_delivered = bbr->delivered;
-		bbr->round_count++;
-		bbr->round_start = true;
-	}
-	else {
-		bbr->round_start = false;
-	}
-} 
-
-/* function called on received ACK */
-void BBRUpdateOnACK(bbr_t* bbr, ctcp_state_t* state, 
-	ctcp_segment_t* segment, uint16_t ackd_packets) {
-	BBRUpdateModelAndState(bbr);
-	BBRUpdateControlParameters(bbr, segment, ackd_packets);
-	BBRUpdateRound(bbr, state, segment); 
-}
-
-void BBRInitRoundCounting(bbr_t* bbr) {
-	bbr->next_round_delivered = 0;
-	bbr->round_start = false;
-	bbr->round_count = 0;
-}
-
-/* initialize a bbr struct with initial variables */
-void bbr_init(bbr_t* bbr, uint16_t recv_window, ctcp_state_t* state) {
 	/* set the variables to initial state */
 	bbr->btl_bw_filter_val = 0;
 	bbr->btl_bw_filter_time = 0;
@@ -187,195 +20,158 @@ void bbr_init(bbr_t* bbr, uint16_t recv_window, ctcp_state_t* state) {
 	}
 	bbr->cwnd = bbr->initial_cwnd;
 
-	BBREnterStartup(bbr); 
-	BBRInitRoundCounting(bbr);
-	BBRInitFullPipe(bbr);
-	BBRInitPacingRate(bbr, recv_window);
-	
+	bbr->rtt_cnt = 0;
+	bbr->mode = BBR_STARTUP;
+	bbr->min_rtt_win_sec = 10;
+	bbr->rtt_m_win = 10000;
+	bbr->btlbw = 1440;
+	bbr->current_pacing_gain = bbr_pacing_gain[0];
+	bbr->next_packet_send_time = 0;
+	bbr->rtt_prop = 200;
+
+	bbr_enter_startup(bbr);
 }
 
-void BBRCheckFullPipe(bbr_t* bbr) {
-	if(bbr->filled_pipe || !bbr->round_start || bbr->is_app_limited) {
-		/* no need to check */
-		return;
-	}
-	else if(bbr->btl_bw >= bbr->full_bw * 1.25) { /* BW still growing? */
-		bbr->full_bw = bbr->btl_bw;
-		bbr->full_bw_count = 0;
-		return;	
-	}
-	bbr->full_bw_count++;
-	if(bbr->full_bw_count >= 3) {
-		bbr->filled_pipe = true; 
-	}
-}
-
-void BBRCheckDrain(bbr_t* bbr, uint16_t packets_in_flight) {
-	if(bbr->mode == BBR_STARTUP && bbr->filled_pipe) {
-		BBREnterDrain(bbr);
-		return;
-	}
-	if(bbr->mode == BBR_DRAIN && packets_in_flight <= BBRInFlight(1.0)) {
-		BBREnterProbeBW(bbr); 
-	}
-}
-
-// source: https://www.geeksforgeeks.org/generating-random-number-range-c/
-void random_int_in_range(int lower, int upper) 
-{ 
-	return (rand() % (upper - lower + 1)) + lower; 
-} 
-
-void BBREnterProbeBW(bbr_t* bbr) {
-    bbr->state = BBR_PROBE_BW; 
-    bbr->pacing_gain = 1;
-    bbr->cwnd_gain = 2;
-    bbr->cycle_index = BBRGainCycleLen - 1 - random_int_in_range(0,6)
-    BBRAdvanceCyclePhase(bbr);
-}
-
-void BBRCheckCyclePhase(bbr_t* bbr) {
-	if(bbr->mode == BBR_PROBE_BW && BBRIsNextCyclePhase(bbr, prior_inflight, packets_lost)) {
-		BBRAdvanceCyclePhase(bbr); 
-	}
-}
-
-bool BBRIsNextCyclePhase(bbr_t* bbr, uint32_t prior_inflight, uint32_t packets_lost) {
-    bool is_full_length = (current_time() - bbr->cycle_stamp) > bbr->rt_prop;
-    if(bbr->pacing_gain == 1)
-    	return is_full_length; 
-    if(bbr->pacing_gain > 1) {
-    	return (is_full_length && 
-    	((packets_lost > 0) || prior_inflight >= BBRInFlight(bbr->pacing_gain)));
-    }
-    else  //  (BBR.pacing_gain < 1)
-      	return (is_full_length || (prior_inflight <= BBRInflight(1)));
-}
-
-void BBRAdvanceCyclePhase(bbr_t* bbr) {
-    bbr->cycle_stamp = current_time();
-    bbr->cycle_index = (bbr->cycle_index + 1) % BBRGainCycleLen
-    bbr->pacing_gain = pacing_gain_cycle[bbr->cycle_index];
-}
-
-void BBREnterDrain(bbr_t* bbr) {
-	bbr->mode = BBR_DRAIN; 
-	bbr->pacing_gain = drain_pacing_gain; 	// pace slowly
-	bbr->cwnd_gain = BBRHighGain; 			// maintain cwnd
-}
-
-void BBRInitFullPipe(bbr_t* bbr) {
-	bbr->filled_pipe = false; 
-	bbr->full_bw = 0;
-	bbr->full_bw_count = 0;
-}
-
-void BBREnterStartup(bbr_t* bbr) {
+void bbr_enter_startup(bbr_t* bbr) {
 	bbr->mode = BBR_STARTUP; 
-	bbr->pacing_gain = BBRHighGain; 
-	bbr->cwnd_gain = BBRHighGain; 
-} 
+	bbr->pacing_gain = bbr_high_gain;
+	bbr->cwnd_gain = bbr_high_gain;
 
-/* update the BW max value */
-void BBUpdateBtlBw(bbr_t* bbr, uint32_t delivery_rate, 
-	ctcp_state_t* state, ctcp_segment_t* segment) {
-	// bbr->rtt_cnt++;
-	BBRUpdateRound(bbr, ctcp_state_t* state, ctcp_segment_t* segment); 
-	if(delivery_rate >= bbr->btl_bw || !bbr->is_app_limited) {
-		bbr->btl_bw = max(delivery_rate, bbr->btl_bw); 
+	bbr->delivered_in_round = 0;
+	bbr->rtts_in_mode = 0;
+}
+
+void bbr_enter_drain(bbr_t* bbr) {
+	bbr->mode = BBR_DRAIN; 
+	bbr->pacing_gain = bbr_drain_gain; 
+	bbr->cwnd_gain = bbr_high_gain; 
+
+	bbr->full_bw_cnt = 0;
+	bbr->delivered_in_round = 0;
+	bbr->rtts_in_mode = 0;
+}
+
+void bbr_enter_probe_bw(bbr_t* bbr) {
+	bbr->mode = BBR_PROBE_BW; 
+	bbr->cycle_index = (rand() % (7 - 2 + 1)) + 2; 
+	bbr->pacing_gain = bbr_pacing_gain[bbr->cycle_index]; 
+	bbr->cwnd_gain = bbr_cwnd_gain;
+	bbr->rtts_in_mode = 0;
+	bbr->delivered_in_round = 0;
+}
+
+void bbr_cycle_gain_probe_bw(bbr_t* bbr) {
+	bbr->cycle_index++; 
+	if(bbr->cycle_index > 7) {
+		bbr->cycle_index = 0;
+	}
+
+	bbr->pacing_gain = bbr_pacing_gain[bbr->cycle_index]; 
+}
+
+void bbr_check_probe_rtt(bbr_t* bbr) {
+	bbr->rt_prop_expired = current_time() - bbr->rt_prop_stamp > 10000; 
+	if(bbr->mode != BBR_PROBE_RTT && bbr->rt_prop_expired) {
+		bbr->rt_prop = -1;
+		bbr_enter_probe_rtt(bbr); 
 	}
 }
 
-/* update the RTT min value */
-void BBRUpdateRTprop(bbr_t* bbr, long rtt) {
-	if(current_time() > (bbr->rtprop_stamp + RTpropFilterLen)) {
-		bbr->rtprop_expired = true;
-	}
-	if(rtt >= 0 && (rtt <= bbr->rt_prop || bbr->rtprop_expired)) {
-		bbr->rt_prop = rtt;
-		bbr->rtprop_stamp = current_time();
-		bbr->rtprop_expired = false;
-	}	
+void bbr_enter_probe_rtt(bbr_t* bbr) {
+	bbr->mode = BBR_PROBE_RTT; 
+	bbr->pacing_gain = BBR_UNIT; 
+	bbr->cwnd_gain = BBR_UNIT; 
+	bbr->filled_pipe = false;
+	bbr->rtts_in_mode = 0;
 }
 
-void BBRSetPacingRateWithGain(float pacing_gain) {
-	if(!bbr->pacing_rate_init) {
-		BBRInitPacingRate(bbr, ntohs(segment->window));
+void bbr_set_cwnd(bbr_t* bbr) {
+	bbr->cwnd = bbr->rt_prop * bbr->btl_bw * bbr->pacing_gain; 
+}
+
+void bbr_check_full_pipe(bbr_t* bbr) {
+	if(bbr->filled_pipe) {
 		return;
 	}
-
-	float rate;
-	rate = bbr->pacing_gain * bbr->btl_bw;
-	if(bbr->filled_pipe || (rate > bbr->pacing_rate)) {
-		bbr->pacing_rate = rate;
+	/* BW still growing? */
+	if(bbr->btl_bw >= bbr->full_bw * 1.25) {
+		bbr->full_bw = bbr->btl_bw;
+		bbr->full_bw_cnt = 0;
+	}
+	else {
+		bbr->full_bw_cnt++;
+		if(bbr->full_bw_cnt > 2) {
+			bbr->filled_pipe = true;
+		}
 	}
 }
 
-void BBRHandleRestartFromIdle(bbr_t* bbr, uint32_t packets_in_flight) {
-    if(packets_in_flight == 0 && bbr->is_app_limited) {
-    	bbr->idle_restart = true;
-    	if(bbr->mode == BBR_PROBE_BW) {
-    		BBRSetPacingRateWithGain(1);
-    	}
-    }
-}
+void bbr_update_rtt(bbr_t* bbr, long rtt) {
+	if(rtt < bbr->rt_prop) {
+		bbr->rt_prop = rtt; 
+		bbr->rt_prop_stamp = current_time(); 
 
-/* TODO 
-
-1. translate following sudo code to real code
-2. fix header file to account for all these new functions
-3. implement BBR changes in ctcp.c
-*/
-
-void BBRCheckProbeRTT(bbr_t* bbr, uint32_t packets_in_flight) {
-    if (bbr->mode != BBR_PROBE_BW && bbr->rtprop_expired && !bbr->idle_restart) {
-		BBREnterProbeRTT(bbr);
-		BBRSaveCwnd(bbr); 
-		bbr->probe_rtt_done_stamp = 0;
-  	}
-    if (bbr->mode == ProbeRTT) {
-      BBRHandleProbeRTT(bbr, packets_in_flight); 
-    }
-    bbr->idle_restart = false;
-}
-
-void BBREnterProbeRTT(bbr_t* bbr) {
-    bbr->mode = BBR_PROBE_BW;
-    bbr->pacing_gain = 1;
-    bbr->cwnd_gain = 1;
-}
-
-void BBRHandleProbeRTT(bbr_t* bbr, uint32_t packets_in_flight) {
-    /* Ignore low rate samples during ProbeRTT: */
-    bbr->is_app_limited = (bbr->delivered + packets_in_flight) ? : 1; 
-    if (bbr->probe_rtt_done_stamp == 0 && packets_in_flight <= BBRMinPipeCwnd) {
-		bbr->probe_rtt_done_stamp = current_time() + ProbeRTTDuration; 
-		bbr->probe_rtt_round_done = false; 
-		bbr->next_round_delivered = bbr->delivered;
-  	}
-    else if (bbr->probe_rtt_done_stamp != 0) {
-		if (bbr->round_start) {
-			bbr->probe_rtt_round_done = true;
+		/* done probing, pipe obviously not full */
+		if(bbr->mode == BBR_PROBE_RTT) {
+			bbr_enter_startup(bbr); 
 		}
-		if (bbr->probe_rtt_round_done && current_time() > bbr->probe_rtt_done_stamp) {
-			bbr->rtprop_stamp = current_time();
-			BBRRestoreCwnd(bbr);
-			BBRExitProbeRTT(bbr);
-		}
-    }
+	}
 }
 
-void BBRExitProbeRTT(bbr_t* bbr) {
-    if (bbr->filled_pipe) {
-      BBREnterProbeBW(bbr);
-    }
-    else {
-      BBREnterStartup(bbr); 
-    }
+
+void bbr_update_btl_bw(bbr_t* bbr, long rtt, uint32_t seg_len) {
+	if(rtt <= 0) {
+		fprintf(stderr, "RTT is negative or == 0\n"); 
+
+		return;
+	}
+	bbr->btl_bw = seg_len / rtt; 
+	bbr->full_bw = max(bbr->btl_bw, bbr->full_bw);
 }
 
-uint32_t BBROnTransmit(bbr_t* bbr) {
-	BBRHandleRestartFromIdle(bbr);
-	return bbr->delivered;
+
+void bbr_on_ack(bbr_t* bbr, long rtt, uint32_t seg_len) {
+
+	bbr->rtt_cnt++;
+
+	bbr_update_btl_bw(bbr, rtt, seg_len); 
+	bbr_check_probe_rtt(bbr); 
+	bbr_check_full_pipe(bbr); 
+	bbr_set_cwnd(bbr);
+	
+	switch(bbr->mode) {
+		case BBR_STARTUP:
+
+			if(bbr->filled_pipe) {
+				bbr_enter_drain(bbr); 
+			}
+			break;
+		case BBR_DRAIN:
+			bbr->rtts_in_mode++;
+			/* Drain the queues for 5 RTT's */
+			if(rtts_in_mode > 5) {
+				bbr_enter_probe_bw(bbr); 
+			}
+			break; 
+		case BBR_PROBE_BW:
+
+			bbr_cycle_gain_probe_bw(bbr); 
+			break; 
+		case BBR_PROBE_RTT:
+			bbr->rtts_in_mode++;
+			if(bbr->filled_pipe) {
+				bbr_enter_probe_bw(bbr); 
+			}
+			if(bbr->rtts_in_mode > 7) {
+				bbr_enter_startup(bbr); 
+			}
+			break;
+		default: 
+			fprintf(stderr, "State Transition Error, going to drain state. \n");
+			bbr_enter_drain(bbr); 
+			break;
+	}
+
+	bbr_update_rtt(bbr, rtt); 
 }
 
--- lab3/ctcp_bbr.h
+++ lab3/ctcp_bbr.h
@@ -46,10 +46,10 @@ long ProbeRTTDuration = 200;		/* A constant specifying the minimum duration for 
 									fewer packets: 200 ms.*/
 uint32_t SMSS = 1500;				/* The Sender Maximum Segment Size. */
 
-/* Gain constants */
-static float BBRHighGain = 2.89; /* (2/ln(2) ~= 2.89) */
+/* Gain constants
+static float BBRHighGain = 2.89; 
 static float drain_pacing_gain = 1/2.89;
-float pacing_gain_cycle[] = {5/4, 3/4, 1, 1, 1, 1, 1, 1};
+float pacing_gain_cycle[] = {5/4, 3/4, 1, 1, 1, 1, 1, 1};*/
 
 
 /* We use a high_gain value chosen to allow a smoothly increasing pacing rate
@@ -63,7 +63,6 @@ static int bbr_cwnd_gain  = BBR_UNIT * 2;	/* gain for steady-state cwnd */
 static int bbr_pacing_gain[] = { BBR_UNIT * 5 / 4, BBR_UNIT * 3 / 4,
 				 BBR_UNIT, BBR_UNIT, BBR_UNIT,
 				 BBR_UNIT, BBR_UNIT, BBR_UNIT };
-static uint32_t bbr_cycle_rand = 7;  /* randomize gain cycling phase over N phases */
 
 /* INET_DIAG_BBRINFO */
 typedef struct {
@@ -94,10 +93,7 @@ typedef struct {
 								scheduled and transmitted together.*/
 	uint16_t 	cwnd; 			/* the sender's congestion window */
 	uint32_t 	btl_bw;			/* BBR's current estimate of btlBW */
-	uint32_t	btl_bw_filter_val;	/* max filter used to estimate btl_bw */
-	long 		btl_bw_filter_time;	
-	uint32_t 	full_bw; 
-	uint16_t 	full_bw_count; 
+	uint32_t 	full_bw;		/* full BW estimate */
 	long 		rt_prop; 		/* BBR's estimated two-way round-trip propagation
 								 delay of the path, estimated from the windowed minimum
 								 recent round-trip delay sample.*/
@@ -108,120 +104,20 @@ typedef struct {
 	float 		pacing_gain;	/* gain factor used to scale BBR.BtlBw to produce BBR.pacing_rate.*/
 	float 		cwnd_gain; 		/* The dynamic gain factor used to scale the estimated 
 								BDP to produce a congestion window (cwnd). */
-	uint16_t 	initial_cwnd; 	/* initial value of cwnd given at initialization */
-	uint16_t 	target_cwnd;	/* cwnd that you want to go towards */
 	bool 		filled_pipe;	/* BBR's estimate, whether it has ever filled the pipe */
-	uint16_t	round_count;	/* count of packet times round trips */
-	bool 		round_start; 	/*A boolean that BBR sets to true once per packet-timed 
-								round trip, on ACKs that advance BBR.round_count.*/
-	uint32_t	next_round_delivered;/* packet.delivered value denoting the end of a 
-								packet-timed round trip.*/
-	uint32_t	delivered;		/* number of packets delivered by BBR */
-	bool 		is_app_limited; /* bool to track if you are app limited */
-	bool 		pacing_rate_init;/* to track whether the pacing rate has been initialized */
-	bool 		packet_conservation; /* whether youre in packet conservation mode */
-	uint16_t 	probe_rtt_done_stamp; /* stamp of when we are probing for RTT */
+	uint32_t	delivered_in_round;		/* number of packets delivered by BBR in current round */
+	long	 	probe_rtt_done_stamp; /* stamp of when we are done probing for RTT */
 	bool 		probe_rtt_round_done; /* whether we are done with Probe RTT */
 	bool 		idle_restart; 
-	uint16_t 	cycle_index; 
+	uint16_t	full_bw_cnt; 	/* how many times there is no BW improvement */
+	uint8_t 	cycle_index; 	/* pacing gain index for probe_bw*/
+	uint32_t	rtt_count; 		/* need to update BW estimate every 10 */
+	uint8_t		rtts_in_mode;  /* number of RTT's spent in the phase */
 } bbr_t;
 
-void BBRUpdateModelAndState(bbr_t* bbr, uint32_t delivery_rate);
-void BBRUpdateBtlBw(bbr_t* bbr, uint32_t delivery_rate);
-void BBRInitPacingRate(bbr_t* bbr, uint16_t InitialCwnd);
-void BBRSetPacingRate(bbr_t* bbr, ctcp_segment_t* segment);
-void BBRModulateCwndForProbeRTT(bbr_t* bbr);
-uint16_t BBRSaveCwnd(bbr_t* bbr);
-
-void BBRUpdateOnRTO(bbr_t* bbr);
-/* 
-TODO: implement these functions: 
-void uponEnteringFastRecovery()
-  BBR.prior_cwnd = BBRSaveCwnd()
-  cwnd = packets_in_flight + max(packets_delivered, 1)
-  BBR.packet_conservation = true
-
-void onACKinFastRecovery()
-BBRModulateCwndForRecovery():
-    if (packets_lost > 0)
-      cwnd = max(cwnd - packets_lost, 1)
-    if (BBR.packet_conservation)
-      cwnd = max(cwnd, packets_in_flight + packets_delivered)
-
-After one trip in fast recovery: 
-  BBR.packet_conservation = false
-
-After exiting fast recovery:
-	BBR.packet_conservation = false
-	BBRRestoreCwnd()
-*/
-
-void BBRSetSendQuantum(bbr_t* bbr);
-void BBRSetCwnd(bbr_t* bbr, uint16_t ackd_packets);
-uint16_t BBRInFlight(bbr_t* bbr, float gain);
-void BBRUpdateTargetCwnd(bbr_t* bbr);
-void BBRUpdateControlParameters(bbr_t* bbr, ctcp_segment_t* segment, uint16_t ackd_packets);
-void BBRUpdateRound(bbr_t* bbr, ctcp_state_t* state, ctcp_segment_t* segment);
-/* function called on received ACK */
-void BBRUpdateOnACK(bbr_t* bbr, ctcp_state_t* state, 
-	ctcp_segment_t* segment, uint16_t ackd_packets);
-void BBRInitRoundCounting(bbr_t* bbr);
-/* initialize a bbr struct with initial variables */
-void bbr_init(bbr_t* bbr, uint16_t recv_window, ctcp_state_t* state);
-void BBRCheckFullPipe(bbr_t* bbr);
-void BBRCheckDrain(bbr_t* bbr, uint16_t packets_in_flight);
-// source: https://www.geeksforgeeks.org/generating-random-number-range-c/
-void random_int_in_range(int lower, int upper);
-void BBREnterProbeBW(bbr_t* bbr);
-void BBRCheckCyclePhase(bbr_t* bbr);
-bool BBRIsNextCyclePhase(bbr_t* bbr, uint32_t prior_inflight, uint32_t packets_lost);
-void BBRAdvanceCyclePhase(bbr_t* bbr);
-
-void BBREnterDrain(bbr_t* bbr);
-void BBRInitFullPipe(bbr_t* bbr);
-void BBREnterStartup(bbr_t* bbr);
-/* update the BW max value */
-void BBUpdateBtlBw(bbr_t* bbr, uint32_t delivery_rate, 
-	ctcp_state_t* state, ctcp_segment_t* segment);
-/* update the RTT min value */
-void BBRUpdateRTprop(bbr_t* bbr, long rtt);
-void BBRSetPacingRateWithGain(float pacing_gain);
-void BBRHandleRestartFromIdle(bbr_t* bbr, uint32_t packets_in_flight);
-void BBRCheckProbeRTT(bbr_t* bbr, uint32_t packets_in_flight);
-void BBREnterProbeRTT(bbr_t* bbr);
-void BBRHandleProbeRTT(bbr_t* bbr, uint32_t packets_in_flight);
-void BBRExitProbeRTT(bbr_t* bbr);
-uint32_t BBROnTransmit(bbr_t* bbr);
+void bbr_init(bbr_t* bbr);
+void bbr_on_ack(bbr_t* bbr);
+void bbr_update_rtt(bbr_t* bbr, long rtt);
+void bbr_update_btl_bw(bbr_t* bbr); 
 
 #endif
-
-// old BBR Block
-// uint32_t	min_rtt_ms;	        		/* min RTT in min_rtt_win_sec window */
-// 	long		min_rtt_stamp;	        	/* timestamp of min_rtt_ms */
-// 	uint32_t	probe_rtt_done_stamp;   	/* end time for BBR_PROBE_RTT mode */
-// 	uint32_t	rtt_cnt;	    			/* count of packet-timed rounds elapsed */
-// 	uint32_t    next_rtt_delivered; 		/* scb->tx.delivered at end of round */
-// 	bbr_mode 	mode,		     /* current bbr_mode in state machine */
-// 	uint32_t	prev_ca_state:3,     /* CA state on previous ACK */
-// 		packet_conservation:1,  /* use packet conservation? */
-// 		restore_cwnd:1,	     /* decided to revert cwnd to old value */
-// 		round_start:1,	     /* start of packet-timed tx->ack round? */
-// 		tso_segs_goal:7,     /* segments we want in each skb we send */
-// 		idle_restart:1,	     /* restarting after idle? */
-// 		probe_rtt_round_done:1,  /* a BBR_PROBE_RTT round at 4 pkts? */
-// 		unused:5,
-// 		lt_is_sampling:1,     taking long-term ("LT") samples now? 
-// 		lt_rtt_cnt:7,	     /* round trips in long-term interval */
-// 		lt_use_bw:1;	     /* use lt_bw as our bw estimate? */
-// 	uint32_t	lt_bw;		     /* LT est delivery rate in pkts/uS << 24 */
-// 	uint32_t	lt_last_delivered;   /* LT intvl start: tp->delivered */
-// 	uint32_t	lt_last_stamp;	     /* LT intvl start: tp->delivered_mstamp */
-// 	uint32_t	lt_last_lost;	     /* LT intvl start: tp->lost */
-// 	uint32_t	pacing_gain,	/* current gain for setting pacing rate */
-// 		cwnd_gain,	/* current gain for setting cwnd */
-// 		full_bw_cnt,	/* number of rounds without large bw gains */
-// 		cycle_idx;	/* current index in pacing_gain cycle array */
-// 	uint32_t	prior_cwnd;	/* prior cwnd upon entering loss recovery */
-// 	uint32_t	full_bw;	/* recent bw, to estimate if pipe is full, 
-// 							take 90% of this value */
-// 	uint32_t 	data_sent_in_phase 			/* data sent during this mode 
